de23d6114bb7c3b6feaddb5f88a3eb5d54082b64
SAMIM AKTHER <luckyluck83689@gmail.com>
2024-06-17 17:49:42 +0530

    Tr

--- Questions/question2.java
+++ Questions/question2.java
@@ -1,8 +1,8 @@
 package Questions;
 
 public class question2 {
-    /*Given n non-negative integers representing an elevation map
-     where the width of each bar is 1, compute how much water it can trap after raining.
+/*Given n non-negative integers representing an elevation map
+ where the width of each bar is 1, compute how much water it can trap after raining.
 
 Example 1:
 Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
@@ -45,4 +45,5 @@ public static int Trappedwater_afterraining(int height[]){
         return trappedwater;
    
 }
+    
 }
